e7ce743259dce4f20382f1b869b98be59c8bad3e
asvinb <asvin.balloo@gmail.com>
2025-09-01 19:40:41 +0400

    Add Notice component.

--- js/src/components/reddit-combo-account-card/account-details.js
+++ js/src/components/reddit-combo-account-card/account-details.js
@@ -8,45 +8,14 @@ import { __ } from '@wordpress/i18n';
  */
 import useRedditAccountConfig from '~/hooks/useRedditAccountConfig';
 import './account-details.scss';
-import AppNotice from '../app-notice';
-import AppButton from '../app-button';
 
 const AccountDetails = () => {
 	const {
-		business_id: businessId,
 		business_name: businessName,
 		ad_account_id: adsAccountId,
 		ad_account_name: adsAccountName,
 		pixel_id: pixelId,
 	} = useRedditAccountConfig();
-
-	if ( ! businessId && ! adsAccountId && ! pixelId ) {
-		return (
-			<AppNotice
-				status="warning"
-				isDismissible={ false }
-				className="rfw-reddit-account-details__notice"
-			>
-				<p>
-					{ __(
-						"We couldn't find a Reddit Business Account connected to your user.",
-						'reddit-for-woo'
-					) }
-				</p>
-				<AppButton
-					isPrimary
-					text={ __( 'Create Business Account', 'reddit-for-woo' ) }
-					onClick={ () => {
-						window.open(
-							'https://accounts.reddit.com/adsregister',
-							'_blank',
-							'noopener,noreferrer'
-						);
-					} }
-				/>
-			</AppNotice>
-		);
-	}
 
 	return (
 		<div className="rfw-reddit-account-details">
