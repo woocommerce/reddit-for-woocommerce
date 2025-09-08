849ce43c827e2bfafb781894c49809eef77b17bb
asvinb <asvin.balloo@gmail.com>
2025-09-08 12:09:36 +0400

    Remove pixel account connection UI.

--- js/src/components/reddit-combo-account-card/connect-ads/connect-ads.js
+++ js/src/components/reddit-combo-account-card/connect-ads/connect-ads.js
@@ -1,13 +1,7 @@
 /**
- * External dependencies
- */
-import { useState } from '@wordpress/element';
-
-/**
  * Internal dependencies
  */
 import AccountCard from '~/components/account-card';
-import ConfirmCreateModal from './confirm-create-modal';
 import ConnectExistingAccount from './connect-existing-account';
 import UpsertingAccount from './upserting-account';
 
@@ -15,42 +9,15 @@ import UpsertingAccount from './upserting-account';
  * ConnectAds component renders an account card to connect to an existing Reddit Ads account.
  *
  * @param {Object} props Component props.
- * @param {Function} props.onRequestCreate A callback to fire when creating a new account.
  * @param {string|null} props.upsertingAction The action the user is performing. Possible values are 'create', 'update', or null.
  * @return {JSX.Element} {@link AccountCard} filled with content.
  */
-const ConnectAds = ( { onRequestCreate, upsertingAction } ) => {
-	const [ showCreateNewModal, setShowCreateNewModal ] = useState( false );
-
+const ConnectAds = ( { upsertingAction } ) => {
 	if ( upsertingAction ) {
 		return <UpsertingAccount upsertingAction={ upsertingAction } />;
 	}
 
-	const handleCreateClick = () => {
-		setShowCreateNewModal( true );
-	};
-
-	const handleRequestClose = () => {
-		setShowCreateNewModal( false );
-	};
-
-	const handleContinue = () => {
-		onRequestCreate();
-		handleRequestClose();
-	};
-
-	return (
-		<>
-			<ConnectExistingAccount onCreateClick={ handleCreateClick } />
-
-			{ showCreateNewModal && (
-				<ConfirmCreateModal
-					onContinue={ handleContinue }
-					onRequestClose={ handleRequestClose }
-				/>
-			) }
-		</>
-	);
+	return <ConnectExistingAccount />;
 };
 
 export default ConnectAds;
