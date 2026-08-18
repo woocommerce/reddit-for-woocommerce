8551a80d72425c083eb9ada3aacaa87a88ce3eca
Alejandro Perez Martin <aleperez92@gmail.com>
2026-08-18 13:52:46 +0200

    Adjust layout to make select and label aligned

--- js/src/meta-boxes/channel-visibility/channel-visibility-settings.js
+++ js/src/meta-boxes/channel-visibility/channel-visibility-settings.js
@@ -58,99 +58,97 @@ const ChannelVisibilitySettings = () => {
 
 	return (
 		<Flex direction="column" gap={ 4 } className="rfw-channel-visibility">
-			<Flex direction="column" gap={ 4 }>
-				<FlexBlock>
-					<Flex gap={ 2 } align="center" justify="flex-start">
-						<FlexItem>
-							<Flex gap={ 2 } align="center">
-								<FlexItem>
-									<img
-										className="rfw-channel-visibility__logo"
-										src={ redditLogoURL }
-										alt={ __(
-											'Reddit Logo',
-											'reddit-for-woocommerce'
-										) }
-										width={ 16 }
-										height={ 16 }
-									/>
-								</FlexItem>
-								<FlexItem>
-									{ __( 'Reddit', 'reddit-for-woocommerce' ) }
-								</FlexItem>
-							</Flex>
-						</FlexItem>
-
-						<FlexBlock>
-							<SelectControl
-								aria-label={ __(
-									'Channel visibility setting',
-									'reddit-for-woocommerce'
-								) }
-								name={ fieldName }
-								options={ syncOptions }
-								value={ value }
-								onChange={ setValue }
-								disabled={ ! productIsVisible }
-								__nextHasNoMarginBottom
-							/>
-						</FlexBlock>
-					</Flex>
-				</FlexBlock>
-
-				{ ! productIsVisible && (
+			<FlexBlock>
+				<Flex gap={ 2 } align="center" justify="flex-start">
 					<FlexBlock>
-						<Notice status="info" isDismissible={ false }>
-							<p>
-								{ __(
-									'This product cannot be shown on any channel because it is hidden from your store catalog.',
-									'reddit-for-woocommerce'
-								) }
-							</p>
-						</Notice>
-					</FlexBlock>
-				) }
-
-				{ shouldDisplaySyncNotice && syncStatusText && (
-					<FlexBlock>
-						<Notice
-							className="rfw-channel-visibility__sync-notice"
-							isDismissible={ false }
-							status={ hasIssues ? 'warning' : 'info' }
-						>
-							<p>
-								<strong>
-									{ __(
-										'Reddit sync status',
+						<Flex gap={ 2 } align="center" justify="flex-start">
+							<FlexItem>
+								<img
+									className="rfw-channel-visibility__logo"
+									src={ redditLogoURL }
+									alt={ __(
+										'Reddit Logo',
 										'reddit-for-woocommerce'
 									) }
-								</strong>
-							</p>
-							<p className="rfw-channel-visibility__sync-status">
-								{ syncStatusText }
-							</p>
-
-							{ hasIssues && (
-								<>
-									<p>
-										<strong>
-											{ __(
-												'Issues',
-												'reddit-for-woocommerce'
-											) }
-										</strong>
-									</p>
-									<ul>
-										{ issues.map( ( issue ) => (
-											<li key={ issue }>{ issue }</li>
-										) ) }
-									</ul>
-								</>
-							) }
-						</Notice>
+									width={ 16 }
+									height={ 16 }
+								/>
+							</FlexItem>
+							<FlexItem>
+								{ __( 'Reddit', 'reddit-for-woocommerce' ) }
+							</FlexItem>
+						</Flex>
 					</FlexBlock>
-				) }
-			</Flex>
+
+					<FlexItem>
+						<SelectControl
+							aria-label={ __(
+								'Channel visibility setting',
+								'reddit-for-woocommerce'
+							) }
+							name={ fieldName }
+							options={ syncOptions }
+							value={ value }
+							onChange={ setValue }
+							disabled={ ! productIsVisible }
+							__nextHasNoMarginBottom
+						/>
+					</FlexItem>
+				</Flex>
+			</FlexBlock>
+
+			{ ! productIsVisible && (
+				<FlexBlock>
+					<Notice status="info" isDismissible={ false }>
+						<p>
+							{ __(
+								'This product cannot be shown on any channel because it is hidden from your store catalog.',
+								'reddit-for-woocommerce'
+							) }
+						</p>
+					</Notice>
+				</FlexBlock>
+			) }
+
+			{ shouldDisplaySyncNotice && syncStatusText && (
+				<FlexBlock>
+					<Notice
+						className="rfw-channel-visibility__sync-notice"
+						isDismissible={ false }
+						status={ hasIssues ? 'warning' : 'info' }
+					>
+						<p>
+							<strong>
+								{ __(
+									'Reddit sync status',
+									'reddit-for-woocommerce'
+								) }
+							</strong>
+						</p>
+						<p className="rfw-channel-visibility__sync-status">
+							{ syncStatusText }
+						</p>
+
+						{ hasIssues && (
+							<>
+								<p>
+									<strong>
+										{ __(
+											'Issues',
+											'reddit-for-woocommerce'
+										) }
+									</strong>
+								</p>
+								<ul>
+									{ issues.map( ( issue ) => (
+										<li key={ issue }>{ issue }</li>
+									) ) }
+								</ul>
+							</>
+						) }
+					</Notice>
+				</FlexBlock>
+			) }
 		</Flex>
 	);
 };
